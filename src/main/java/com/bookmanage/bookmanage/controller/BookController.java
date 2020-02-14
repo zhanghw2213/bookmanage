e369a13ff530300bd28138ac9e1c80e9e932109f
zhanghw2213 <hongwei.zhang@fenbeitong.com>
2020-02-14 16:07:12 +0800

    静态资源文件映射

--- src/main/java/com/bookmanage/bookmanage/controller/BookController.java
+++ src/main/java/com/bookmanage/bookmanage/controller/BookController.java
@@ -1,4 +1,18 @@
 package com.bookmanage.bookmanage.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    @RequestMapping(value = "/submit", method = RequestMethod.PUT)
+    public JSONObject getAccount(String tittle, String authorName, String Source, String fileList) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result",true);
+        return jsonObject;
+    }
 }
--- src/main/resources/assets/js/upload.js
+++ src/main/resources/assets/js/upload.js
@@ -1,5 +1,5 @@
 $("#file-input").fileinput({
-    uploadUrl: "/file",
+    uploadUrl: "/file/upload",
     uploadAsync: true,
     maxFileCount: 100,
     previewFileType: ['image', 'html', 'text', 'video', 'audio', 'flash'],
@@ -14,7 +14,8 @@ $("#file-input").fileinput({
     maxFilePreviewSize: 51200
 }).on('fileuploaded', function (event, data, previewId, index) {
     var json = data.response;
-    if (json.status === "success") {
+    console.log(json)
+    if (json.result === "success") {
         alerts("上传成功");
     } else {
         alerts("上传失败，文件不合法");
@@ -37,3 +38,5 @@ $.get("/category/all", function (data) {
         $("#category-id").html(option);
     }
 });
+
+
